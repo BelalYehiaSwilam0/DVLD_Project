51e61ca668c20c4a1b26ffdd2acbf83609ef413d
Belal Yehia Swilam <belal.yehia.swilam1@gmail.com>
2026-04-22 04:37:13 +0200

    refactor: cleanup project structure and prune unused assets

--- DVLD/Login/frmLoginToYourAccountcs.Designer.cs
+++ DVLD/Login/frmLoginToYourAccountcs.Designer.cs
@@ -187,7 +187,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pictureBox1.Image = global::DVLD.Properties.Resources.IconLogo;
+           
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(442, 308);
